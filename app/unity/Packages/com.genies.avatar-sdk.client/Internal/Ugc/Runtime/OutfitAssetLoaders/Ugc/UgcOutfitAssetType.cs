ab7ed0e672714f001071105e3ad305193ecb52e9
elshaddai melaku <109170141+melakueb@users.noreply.github.com>
2026-03-19 14:57:15 -0400

    Delete app/unity directory

--- app/unity/Packages/com.genies.avatar-sdk.client/Internal/Ugc/Runtime/OutfitAssetLoaders/Ugc/UgcOutfitAssetType.cs
+++
@@ -1,9 +0,0 @@
-namespace Genies.Ugc
-{
-    public static class UgcOutfitAssetType
-    {
-        public const string Ugc = "ugc";
-        public const string UgcDefault = "ugc-default";
-        public const string Gear = "gear";
-    }
-}
--- app/unity/Packages/com.genies.avatar-sdk.client/Internal/Ugc/Runtime/StyleService/StyleServiceStates.cs
+++
@@ -1,7 +0,0 @@
-namespace Genies.Ugc
-{
-    public class StyleServiceStates
-    {
-        public const string CustomStyle = "CustomStyle";
-    }
-}
