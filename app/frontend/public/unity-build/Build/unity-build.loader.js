b030915884612b95765e09a959ea2f2156795d97
khanfareena <khansfareena@gmail.com>
2026-04-03 11:37:16 -0400

    rebuild unity webgl build output (bc we changed unity source files SignController.cs, ASL_Controller.controller, SampleScene.unity)

--- app/frontend/public/unity-build/Build/unity-build.loader.js
+++ app/frontend/public/unity-build/Build/unity-build.loader.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9e8562a0560ab6c7ab5b6141ab584a9d52d9d4b76b0f2398e6fdd3a870e94d1
+oid sha256:871ac1f6221ec5aab04b086eabd3fa631962c952c3cedfbe02ba0d3043652e9e
 size 25778
