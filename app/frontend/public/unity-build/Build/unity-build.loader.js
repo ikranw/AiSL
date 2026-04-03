18fbcaceb1c7e81322eab09ededa8edc3336e5c9
khanfareena <khansfareena@gmail.com>
2026-04-03 19:22:08 -0400

    rebuild webgl

--- app/frontend/public/unity-build/Build/unity-build.loader.js
+++ app/frontend/public/unity-build/Build/unity-build.loader.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:871ac1f6221ec5aab04b086eabd3fa631962c952c3cedfbe02ba0d3043652e9e
+oid sha256:ee68f8e9d2524a34a4e907ba58181dda525494255025fd8bd4dae100006877a0
 size 25778
